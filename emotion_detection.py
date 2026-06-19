521179d0a3f149e8824da3ab7a28f0423463f770
Joseph-RIL <joseph.abraham@ril.com>
2026-06-19 05:19:03 -0400

    Updated output formatting

--- emotion_detection.py
+++ emotion_detection.py
@@ -1,4 +1,5 @@
 import requests
+import json
 
 def emotion_detector(text_to_analyze):
     url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
@@ -6,5 +7,19 @@ def emotion_detector(text_to_analyze):
     data = { "raw_document": { "text": text_to_analyze } }
 
     response = requests.post(url, headers=headers, json=data)
+    refined_response = json.loads(response.text)
+    emotional_response = refined_response['emotionPredictions'][0]['emotion']
 
-    return response.text
+    response_data = {
+        'anger': emotional_response['anger'],
+        'disgust': emotional_response['disgust'],
+        'fear': emotional_response['fear'],
+        'joy': emotional_response['joy'],
+        'sadness': emotional_response['sadness']
+    }
+
+    dominant_emotion = max(response_data, key=response_data.get)
+    
+    response_data['dominant_emotion'] = dominant_emotion
+
+    return response_data
